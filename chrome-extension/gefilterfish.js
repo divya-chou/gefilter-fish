4fca992784afd85502b6c6fcc12b85c4c4097c3b
Patrick Lestrange <plestran@uw.edu>
2018-01-25 10:34:54 -0800

    cleaning up nmf model and chrome extension

--- chrome-extension/gefilterfish.js
+++ chrome-extension/gefilterfish.js
@@ -1,33 +1,33 @@
 // Grab the ASIN and product title
 var asin         = document.getElementById("ASIN").value;
 var productTitle = document.getElementById("productTitle").innerText;
+console.log("doing something");
 
 // Check if ASIN is in our database
-$.get( "https://localhost:5000/in_db", { asin: asin })
+$.get( "https://gefilterfish.science/in_db", { asin: asin })
   .done( function ( in_db ) {
 
     // If in the DB then modify the page
     if (in_db) {
 
       // Grab the topics and add in the buttons
-      $.get( "https://localhost:5000/model", { asin: asin })
+      $.get( "https://gefilterfish.science/model", { asin: asin })
         .done( function( data ) { 
 
         // Replace the title of the section
         $('h3[data-hook="lighthut-title"]').replaceWith('<h3 data-hook="lighthut-title" class="a-spacing-base">Topics in customer reviews</h3>');
      
           // Add in buttons
-          console.log(data.topic.length); 
-          $('.cr-lighthouse-terms').replaceWith('<div class="cr-lighthouse-terms"><div float="left">');
+          console.log("got here");
+          $('.cr-lighthouse-terms').replaceWith('<div class="cr-lighthouse-terms">');
           for (var i = 0; i < data.topic.length; i++) {
-            $('.cr-lighthouse-terms').append('<span class="a-declarative">'+
-                                                '<form action="https://127.0.0.1:5000/reviews?topic='+i+'&title='+encodeURIComponent(productTitle)+'" method="post">'+
-                                                 ' <button class="cr-lighthouse-term" name="submit" type="submit">'+data.topic[i]+'</button>'+
-                                                '</form>'+
-                                              '</span>');
+            $('.cr-lighthouse-terms').append('<form action="https://gefilterfish.science/reviews?topic='+i+'&title='+encodeURIComponent(productTitle)+'" method="post">'+
+                                               ' <button class="cr-lighthouse-term" name="topic'+i+' type="submit">'+data.topic[i]+'</button>'+
+                                             '</form>');
           }
-          $('.cr-lighthouse-terms').append('</div></div>');
-//          $(".cr-lighthouse-terms").css({"backgroundColor": "black", "color": "white"});
+          $('.cr-lighthouse-terms').append('</div>');
+          $('form').css({"display": "inline"});
+          $('.cr-lighthouse-terms').css({"max-height": "200px"});
       
         });
 
